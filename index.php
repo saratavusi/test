7b68038579276d9cfa50a16fa53917cda742b2cf
pazhadmin <sarah.tavusi@gmail.com>
2018-01-09 11:01:09 +0330

    Initial Commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <meta http-equiv="Content-Type"
       content="text/html;charset=utf-8"/>
 <title>پورتال خبرنامه </title>
-
+sara
 <link rel="stylesheet" href="css/news.css">
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 
